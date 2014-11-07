aaf1245de74ce1f4cd892dbb053e78c44737ce0a
Figue <ffigue@gmail.com>
2014-11-07 20:40:15 +0100

    First version v6

---
+++ build_clean.sh
@@ -0,0 +1,13 @@
+export CROSS_COMPILE=/DATA/android/kernels/gtab/toolchain/arm-unknown-linux-gnueabi-linaro_4.6.4-2013.05/bin/arm-unknown-linux-gnueabi-
+export ARCH=arm
+export KBUILD_BUILD_USER=Figue
+make clean
+make mrproper
+make figue_defconfig
+make menuconfig
+make -j5
+find . -name "*.ko" -exec cp {} ../kernel/system/lib/modules \;
+cp -v arch/arm/boot/zImage ../kernel
+cp -v ../boot.img .
+abootimg -u boot.img -k arch/arm/boot/zImage
+cp -v boot.img ../kernel/
