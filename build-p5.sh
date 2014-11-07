3e568da0981020918fac2b15065991692d892ca4
Figue <ffigue@gmail.com>
2014-11-07 23:04:39 +0100

    clean scripts and configs

--- build-p5.sh
+++ build-p5.sh
@@ -1,9 +1,10 @@
 export CROSS_COMPILE=/DATA/android/kernels/gtab/toolchain/arm-unknown-linux-gnueabi-linaro_4.6.4-2013.05/bin/arm-unknown-linux-gnueabi-
 export ARCH=arm
 export KBUILD_BUILD_USER=Figue
-#make clean
-#make mrproper
-make figue_defconfig
+make clean
+make mrproper
+make figue_p5_defconfig
+#cp -v .config_figue3 .config
 make menuconfig
 make -j5
 find . -name "*.ko" -exec cp {} ../kernel/system/lib/modules \;
